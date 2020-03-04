919415e71c2677bac1f51af3298455baf6c86910
ÁÖÈçµÂ <1320002856@qq.com>
2020-03-04 19:49:46 +0800

    美团外卖网站

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+<style>
+  .el-main{
+    overflow:unset !important;
+    padding:0 !important;
+  }
+</style>
---
+++ src/axios.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://open.duyiedu.com';
+// 添加请求拦截器
+axios.interceptors.request.use(function (config) {
+    // 在发送请求之前做些什么
+    console.log(config);
+    config.params = {
+        ...config.params,
+        appkey:'linRuDe_1573351991753'
+    }
+    return config;
+}, function (error) {
+    // 对请求错误做些什么
+    return Promise.reject(error);
+});
+
+export default axios;
